d6e9fd1a293abb91948dbd76098966e39cf72a55
Sude Nur Komur <sudenurkomur1253@gmail.com>
2024-12-20 03:08:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/concertapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.concertapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.concertapp.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
